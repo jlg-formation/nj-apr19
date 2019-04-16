0e7df9b637ff5d0051fc7c25489ccbd651df93bd
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-04-16 11:21:30 +0200

    ok

--- ws.ts
+++ ws.ts
@@ -2,13 +2,20 @@ import * as express from 'express';
 const app = express.Router();
 export const ws = app;
 
+app.use(express.json());
+app.use(express.urlencoded());
+
+let id = 0;
+const generateId = () => id++;
+
 const users = [];
 
 
 app.get('/clock', (req, res) => res.json({ time: new Date() }));
 
 
-app.post('/user', (req, res) => {
-    ;
-    res.json({});
+app.post('/users?', (req, res) => {
+    const user = { ...req.body, id: generateId() };
+    users.push(user);
+    res.status(201).json(user);
 });
