c7bcdd403726606359b42c150221baf49a497434
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-04-16 13:51:26 +0200

    ok

--- ws.ts
+++ ws.ts
@@ -6,18 +6,31 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 let id = 0;
-const generateId = () => id++;
+const generateId = (): number => id++;
 
-const users = [];
+const users = new Map();
 
 
 app.get('/clock', (req, res) => res.json({ time: new Date() }));
 
 
 app.post('/users?', (req, res) => {
-    const user = { ...req.body, id: generateId() };
-    users.push(user);
+    const id = generateId();
+    const user = { ...req.body, id };
+    users.set(id, user);
     res.status(201).json(user);
 });
 
-app.get('/users?', (req, res) => res.json(users));
+app.get('/users?', (req, res) => res.json(Array.from(users.values())));
+
+app.get('/users?/:id', (req, res) => {
+    if (!users.has(+req.params.id)) {
+        return res.status(404).end();
+    }
+    return res.json(users.get(+req.params.id));
+});
+
+app.delete('/users?', (req, res) => {
+    users.clear();
+    return res.status(204).end();
+});
