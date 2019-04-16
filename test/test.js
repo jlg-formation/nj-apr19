9f6e8deb227039eba987261eca4a7d7252bdf4d2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-04-16 16:08:10 +0200

    ok

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ describe('REST', () => {
         it('should create a user on the system', async function() {
             this.timeout(0);
             try {
-                const response = await axios.get('http://localhostx:9000/ws/users');
+                const response = await axios.get('http://localhost:9000/ws/users');
                 console.log('tototiti', response.data);
                 assert.deepStrictEqual(response.data, []);
             } catch (err) {
