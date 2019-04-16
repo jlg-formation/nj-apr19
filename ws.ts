e4c1d9f764cfb8b53c8975ccc78a3ec3544a2bd8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-04-16 10:55:58 +0200

    ok

--- ws.ts
+++ ws.ts
@@ -2,7 +2,13 @@ import * as express from 'express';
 const app = express.Router();
 export const ws = app;
 
+const users = [];
+
 
 app.get('/clock', (req, res) => res.json({ time: new Date() }));
 
 
+app.post('/user', (req, res) => {
+    ;
+    res.json({});
+});
