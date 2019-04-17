d6da5703be0c69ebf743ada075b3d3156886c16f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-04-17 09:50:57 +0200

    rest generalise

---
+++ rest.ts
@@ -0,0 +1,70 @@
+import * as express from 'express';
+const app = express.Router();
+
+let privateId = 0;
+const generateId = (): number => privateId++;
+
+export class Rest {
+    
+    expose(r: string) {
+        const items = new Map();
+
+        const baseURL = `/${r}s?`;
+        app.post(baseURL, (req, res) => {
+            const id = generateId();
+            const user = { ...req.body, id };
+            items.set(id, user);
+            return res.status(201).json(user);
+        });
+        
+        app.get(baseURL, (req, res) => res.json(Array.from(items.values())));
+        
+        app.get(baseURL + '/:id', (req, res) => {
+            if (!items.has(+req.params.id)) {
+                return res.status(404).end();
+            }
+            return res.json(items.get(+req.params.id));
+        });
+        
+        app.put(baseURL + '/:id', (req, res) => {
+            const id = +req.params.id;
+            if (!items.has(id)) {
+                return res.status(404).end();
+            }
+            items.set(id, { ...req.body, id });
+            return res.status(204).end();
+        });
+        
+        app.patch(baseURL + '/:id', (req, res) => {
+            const id = +req.params.id;
+            if (!items.has(id)) {
+                return res.status(404).end();
+            }
+            items.set(id, { ...items.get(id), ...req.body, id });
+            return res.status(204).end();
+        });
+        
+        app.patch(baseURL + '/:id', (req, res) => {
+            items.forEach((v, k, map) => {
+                map.set(k, { ...map.get(k), ...req.body, id: k });
+            });
+            return res.status(204).end();
+        });
+        
+        app.delete(baseURL, (req, res) => {
+            items.clear();
+            return res.status(204).end();
+        });
+        
+        app.delete(baseURL + '/:id', (req, res) => {
+            if (!items.has(+req.params.id)) {
+                return res.status(404).end();
+            }
+            items.delete(+req.params.id);
+            return res.status(204).end();
+        });
+        return app;
+    }
+    
+}
+
--- ws.ts
+++ ws.ts
@@ -1,14 +1,10 @@
 import * as express from 'express';
+import { Rest } from './rest';
 const app = express.Router();
 export const ws = app;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-let privateId = 0;
-const generateId = (): number => privateId++;
-
-const users = new Map();
 
 app.use((req, res, next) => {
     setTimeout(next, 100);
@@ -17,57 +13,6 @@ app.use((req, res, next) => {
 
 app.get('/clock', (req, res) => res.json({ time: new Date() }));
 
-
-app.post('/users?', (req, res) => {
-    const id = generateId();
-    const user = { ...req.body, id };
-    users.set(id, user);
-    return res.status(201).json(user);
-});
-
-app.get('/users?', (req, res) => res.json(Array.from(users.values())));
-
-app.get('/users?/:id', (req, res) => {
-    if (!users.has(+req.params.id)) {
-        return res.status(404).end();
-    }
-    return res.json(users.get(+req.params.id));
-});
-
-app.put('/users?/:id', (req, res) => {
-    const id = +req.params.id;
-    if (!users.has(id)) {
-        return res.status(404).end();
-    }
-    users.set(id, { ...req.body, id });
-    return res.status(204).end();
-});
-
-app.patch('/users?/:id', (req, res) => {
-    const id = +req.params.id;
-    if (!users.has(id)) {
-        return res.status(404).end();
-    }
-    users.set(id, { ...users.get(id), ...req.body, id });
-    return res.status(204).end();
-});
-
-app.patch('/users?', (req, res) => {
-    users.forEach((v, k, map) => {
-        map.set(k, { ...map.get(k), ...req.body, id: k });
-    });
-    return res.status(204).end();
-});
-
-app.delete('/users?', (req, res) => {
-    users.clear();
-    return res.status(204).end();
-});
-
-app.delete('/users?/:id', (req, res) => {
-    if (!users.has(+req.params.id)) {
-        return res.status(404).end();
-    }
-    users.delete(+req.params.id);
-    return res.status(204).end();
-});
+const resources = ['user', 'bus'];
+const rest = new Rest();
+resources.forEach(r => app.use(rest.expose(r)));
