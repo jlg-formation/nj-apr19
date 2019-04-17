a62c341cfc5b9bf1f0ea34de01c77426c34c1c32
Jean-Louis GUENEGO <jlguenego@gmail.com>
2019-04-17 11:29:27 +0200

    mongo just post

--- rest-db.ts
+++ rest-db.ts
@@ -1,32 +1,32 @@
 import * as express from 'express';
+import { DBConnection } from './db';
+import { InsertOneWriteOpResult } from 'mongodb';
 const app = express.Router();
 
-let privateId = 0;
-const generateId = (): number => privateId++;
+export class RestDB {
 
-export class Rest {
-    
+    db = new DBConnection();
+
     expose(r: string) {
         const items = new Map();
-        
+        const collection = this.db.database.collection(r);
 
         const baseURL = `/${r}s?`;
-        app.post(baseURL, (req, res) => {
-            const id = generateId();
-            const user = { ...req.body, id };
-            items.set(id, user);
+        app.post(baseURL, async (req, res) => {
+            const result: InsertOneWriteOpResult = await collection.insertOne({ ...req.body });
+            const user = { ...req.body, id: result.insertedId };
             return res.status(201).json(user);
         });
-        
+
         app.get(baseURL, (req, res) => res.json(Array.from(items.values())));
-        
+
         app.get(baseURL + '/:id', (req, res) => {
             if (!items.has(+req.params.id)) {
                 return res.status(404).end();
             }
             return res.json(items.get(+req.params.id));
         });
-        
+
         app.put(baseURL + '/:id', (req, res) => {
             const id = +req.params.id;
             if (!items.has(id)) {
@@ -35,7 +35,7 @@ export class Rest {
             items.set(id, { ...req.body, id });
             return res.status(204).end();
         });
-        
+
         app.patch(baseURL + '/:id', (req, res) => {
             const id = +req.params.id;
             if (!items.has(id)) {
@@ -44,19 +44,19 @@ export class Rest {
             items.set(id, { ...items.get(id), ...req.body, id });
             return res.status(204).end();
         });
-        
+
         app.patch(baseURL + '/:id', (req, res) => {
             items.forEach((v, k, map) => {
                 map.set(k, { ...map.get(k), ...req.body, id: k });
             });
             return res.status(204).end();
         });
-        
+
         app.delete(baseURL, (req, res) => {
             items.clear();
             return res.status(204).end();
         });
-        
+
         app.delete(baseURL + '/:id', (req, res) => {
             if (!items.has(+req.params.id)) {
                 return res.status(404).end();
@@ -66,6 +66,6 @@ export class Rest {
         });
         return app;
     }
-    
+
 }
 
--- ws.ts
+++ ws.ts
@@ -1,18 +1,22 @@
 import * as express from 'express';
-import { Rest } from './rest';
-const app = express.Router();
+import { RestDB } from './rest-db';
+export const app = express.Router();
 export const ws = app;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-    setTimeout(next, 100);
+    setTimeout(next, 0);
 });
 
 
 app.get('/clock', (req, res) => res.json({ time: new Date() }));
 
 const resources = ['user', 'bus'];
-const rest = new Rest();
-resources.forEach(r => app.use(rest.expose(r)));
+(async () => {
+    const rest = new RestDB();
+    await rest.db.connect();
+    resources.forEach(r => app.use(rest.expose(r)));
+})();
+
